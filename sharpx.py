bc1d5957ab561010b3b2ed476c84ca508174c267
Ashu2533 <yramasare1234@gmail.com>
2024-10-18 13:27:57 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7225546868:AAGgGmHBKk_5kSoCO4-Z0aRpv6EZTrBVNjc")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("the_devender")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5817935431")
+
+
+
+
+
+  
